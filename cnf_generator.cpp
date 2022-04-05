e1e7af4c79c3b75eca4ee024f52a9ec709bb943a
Sacha Bartholme <sacha.bartholme@hotmail.com>
2022-04-05 19:36:13 -0400

    improved hardness of generated problem by only generating 2-sat

--- cnf_generator.cpp
+++ cnf_generator.cpp
@@ -16,11 +16,12 @@ vector<vector<bool>> CNFGenerator::generate(int n_variables, int n_clauses) {
 
   // Then generate clauses valid according to assignment
   vector<vector<bool>> clauses(n_clauses, vector<bool>(2 * n_variables, false));
+  vector<int> clause_sizes(n_clauses);
 
   int inv_freq = max(int(n_variables / 2), 2);  // can be tuned
   for (int j = 0; j < n_clauses; j++) {
     bool has_valid = false;
-    for (int i = 0; i < n_variables; i++) {
+    for (int i = 0; i < n_variables && clause_sizes[j] < 2; i++) {
       if (RNG::uniform(inv_freq) != 0) continue;
       // if we pass rand(), then include variable i in this clause
 
@@ -28,15 +29,19 @@ vector<vector<bool>> CNFGenerator::generate(int n_variables, int n_clauses) {
       if (has_valid) {
         if (RNG::uniform(inv_freq) != 0) {
           clauses[j][(i << 1)] = true;
+          clause_sizes[j]++;
         } else {
           clauses[j][(i << 1) + 1] = true;
+          clause_sizes[j]++;
         }
       }  // else, we put the actual value
       else {
         if (assignment[i]) {
           clauses[j][(i << 1)] = true;
+          clause_sizes[j]++;
         } else {
           clauses[j][(i << 1) + 1] = true;
+          clause_sizes[j]++;
         }
         has_valid = true;
       }
