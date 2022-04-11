3f19b9149b84d3400c6acd88665052be625bb44c
Sacha Bartholme <sacha.bartholme@hotmail.com>
2022-04-11 15:12:32 -0400

    update main with multi bit solver and final check

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <vector>
 
 #include "cnf_generator.h"
-#include "single_bit_solver.h"
+#include "multi_bit_solver.h"
 
 using namespace std;
 
 int main() {
-  int n_variables = 60;
+  int n_variables = 30;
   int n_clauses = 5 * n_variables;
   // 1000;
 
@@ -19,9 +19,20 @@ int main() {
   // printf("\n\n");
   // }
 
-  SingleBitSolver single_bit_solver = SingleBitSolver(clauses, n_variables);
+  MultiBitSolver multi_bit_solver = MultiBitSolver(clauses, n_variables);
   int periods;
-  vector<bool> assignment = single_bit_solver.solve(periods);
+  vector<bool> assignment = multi_bit_solver.solve(periods);
+
+  // make sure it works
+  bool conj = true;
+  for (vector<int>& clause : clauses) {
+    bool disj = false;
+    for (int u : clause) {
+      disj = disj || ((u & 1) ? !assignment[u >> 1] : assignment[u >> 1]);
+    }
+    conj = conj && disj;
+  }
+  assert(conj);
 
   printf("Assignment found after %d periods: \n", periods);
   for (int i = 0; i < n_variables; i++) {
