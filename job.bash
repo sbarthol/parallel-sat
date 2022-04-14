68f3a5964e69b315424571e3992a10c30f2816dd
Sacha Bartholme <sacha.bartholme@hotmail.com>
2022-04-14 17:12:42 -0400

    add a bash job

---
+++ job.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+./parallel-sat problems/uf250-0100.cnf --multi
+./parallel-sat problems/uf250-076.cnf --multi
+./parallel-sat problems/uf250-077.cnf --multi
+./parallel-sat problems/uf250-078.cnf --multi
+./parallel-sat problems/uf250-079.cnf --multi
+./parallel-sat problems/uf250-080.cnf --multi
+./parallel-sat problems/uf250-081.cnf --multi
+./parallel-sat problems/uf250-082.cnf --multi
+./parallel-sat problems/uf250-083.cnf --multi
+./parallel-sat problems/uf250-084.cnf --multi
+./parallel-sat problems/uf250-085.cnf --multi
+./parallel-sat problems/uf250-086.cnf --multi
+./parallel-sat problems/uf250-087.cnf --multi
+./parallel-sat problems/uf250-088.cnf --multi
+./parallel-sat problems/uf250-089.cnf --multi
+./parallel-sat problems/uf250-090.cnf --multi
+./parallel-sat problems/uf250-091.cnf --multi
+./parallel-sat problems/uf250-092.cnf --multi
+./parallel-sat problems/uf250-093.cnf --multi
+./parallel-sat problems/uf250-094.cnf --multi
+./parallel-sat problems/uf250-095.cnf --multi
+./parallel-sat problems/uf250-096.cnf --multi
+./parallel-sat problems/uf250-097.cnf --multi
+./parallel-sat problems/uf250-098.cnf --multi
+./parallel-sat problems/uf250-099.cnf --multi
