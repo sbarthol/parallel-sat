6971b08e27a3478e56fb6236f6079bacb9ba7b56
Sacha Bartholme <sacha.bartholme@hotmail.com>
2022-04-14 16:43:21 -0400

    improve parser

--- cnf_parser.cpp
+++ cnf_parser.cpp
@@ -14,8 +14,9 @@ vector<vector<int>> CNFParser::parse_file(char* filename) {
   if (!input) {
     printf("Unable to open file: %s.\n", filename);
   }
-
-  fscanf(input, "%i %i", &n_variables, &n_clauses);
+  while (!fscanf(input, "p cnf %i %i", &n_variables, &n_clauses)) {
+    fscanf(input, "%*[^\n]\n");
+  }
 
   vector<vector<int>> clauses(n_clauses);
   for (int i = 0; i < n_clauses; i++) {
