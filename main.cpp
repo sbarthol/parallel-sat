227882df4647dcaa926496b95c64bfb0c6713ec4
Sacha Bartholme <sacha.bartholme@hotmail.com>
2022-04-15 16:38:32 -0400

    change arg order

--- main.cpp
+++ main.cpp
@@ -10,20 +10,20 @@ using namespace std;
 int main(int argc, char* argv[]) {
   if (argc != 3) {
     printf(
-        "Please use the program as \"./parallel-sat <filename> <--single or "
-        "--multi>\"\n");
+        "Please use the program as \"./parallel-sat <--single or --multi> "
+        "<filename>\"\n");
     return -1;
   }
   CNFParser parser;
 
-  std::vector<std::vector<int>> clauses = parser.parse_file(argv[1]);
+  std::vector<std::vector<int>> clauses = parser.parse_file(argv[2]);
   printf("Completed parse of problem, %i n_variables %i n_clauses\n",
          parser.n_variables, parser.n_clauses);
 
   vector<bool> assignment;
   int periods;
 
-  if (!strcmp("--single", argv[2])) {
+  if (!strcmp("--single", argv[1])) {
     printf("Solving using single bit solver...\n");
     clock_t begin = clock();
     SingleBitSolver single_bit_solver =
@@ -32,7 +32,7 @@ int main(int argc, char* argv[]) {
     clock_t end = clock();
     printf("Solution found in %.4lf seconds\n",
            double(end - begin) / CLOCKS_PER_SEC);
-  } else if (!strcmp("--multi", argv[2])) {
+  } else if (!strcmp("--multi", argv[1])) {
     printf("Solving using %lu-bit solver...\n",
            8 * sizeof(MultiBitSolver::uintk_t));
     clock_t begin = clock();
